2b6d3cb1964ffed1d894150f803e5e86cc2ea0e8
saichand24 <71260876+saichand24@users.noreply.github.com>
2023-06-16 22:02:24 +0530

    Routes

--- pages/product/[productID].js
+++ pages/product/[productID].js
@@ -1,4 +1,8 @@
+import { useRouter } from 'next/router';
+
 function ProductDetail() {
-  return <h1>Details about product</h1>;
+  const router = useRouter();
+  const productId = router.query.productId;
+  return <h1>Details about product {productId}</h1>;
 }
 export default ProductDetail;
